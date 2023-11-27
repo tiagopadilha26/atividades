909875edb26bc476cbfffd8cf3fc3e30462def58
tiagopadilha26 <tiagogp26@hotmail.com>
2023-11-27 19:52:19 -0300

    projeto basico corrida de cachorros

---
+++ CorridaDeCachorros/Corredor.cs
@@ -0,0 +1,28 @@
+﻿namespace CorridaDeCachorros;
+
+public class Corredor : BaseModel
+{
+    private static readonly Random Random = new();
+
+    private double _distanciaPercorrida { get; set; }
+    public Posicoes Posicao { get; set; }
+
+    public Corredor(int posicaoCorredor) : base()
+    {
+        Nome = $"Corredor-{posicaoCorredor}";
+        _distanciaPercorrida = 0.0;
+        Posicao = Posicoes.NaoGanho;
+    }
+
+    public void Mover()
+    {
+        int distanciaPercorrida = Random.Next(1, 6);
+
+        _distanciaPercorrida += (distanciaPercorrida * 0.1);
+    }
+
+    public double DistanciaPercorrida()
+    {
+        return _distanciaPercorrida;
+    }
+}
